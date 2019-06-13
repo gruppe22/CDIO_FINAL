d489a9bf764001293ea9aa934ad65fad3ca111ce
Anders Frandsen <anfran@anfran.dk>
2019-06-13 22:06:41 +0200

    renamed file

--- src/dao/IRaavarebatchDAO.java
+++ src/dao/IRaavarebatchDAO.java
@@ -4,7 +4,7 @@ import dto.RaavareBatchDTO;
 
 import java.util.List;
 
-interface IRaavareBatchDAO {
+public interface IRaavareBatchDAO {
     RaavareBatchDTO getRaavareBatch(int rbId) throws DALException;
     List<RaavareBatchDTO> getRaavareBatchList() throws DALException;
     List<RaavareBatchDTO> getRaavareBatchList(int raavareId) throws DALException;
