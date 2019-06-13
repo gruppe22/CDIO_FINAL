4a3ee5eec2e5b9a534ff78888edff3adfac9b0ac
Anders Frandsen <anfran@anfran.dk>
2019-06-13 23:45:14 +0200

    DAO methods in VaegtLogic

--- src/logic/VaegtLogic.java
+++ src/logic/VaegtLogic.java
@@ -1,8 +1,27 @@
 package logic;
 
-import java.io.*;
-import java.net.Socket;
+import dao.*;
+import dto.*;
 
 public class VaegtLogic {
+    private IProduktBatchDAO batchDAO = new ProduktBatchDAO();
+    private IReceptDAO receptDAO = new ReceptDAO();
 
+    public ReceptDTO getRecept(int receptId) {
+        try {
+            return receptDAO.getRecept(receptId);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    public ProduktBatchDTO getProduktBatch(int batchId) {
+        try {
+            return batchDAO.getProduktBatch(batchId);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
 }
