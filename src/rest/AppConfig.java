65deb9898dab77f7c115b314348d71cf3498f82b
Anders Frandsen <anfran@anfran.dk>
2019-06-14 19:33:24 +0200

    æsadfs

--- src/rest/AppConfig.java
+++ src/rest/AppConfig.java
@@ -2,8 +2,9 @@ package rest;
 
 import org.glassfish.jersey.server.ResourceConfig;
 import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
 
 @ApplicationPath("/rest")
-public class AppConfig extends ResourceConfig {
+public class AppConfig extends Application {
 
 }
