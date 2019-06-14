a612e2624fdec516ec7248676fe7e1550565e383
Anders Frandsen <anfran@anfran.dk>
2019-06-14 15:50:40 +0200

    rest AppConfig extending ResourceConfig

--- src/rest/AppConfig.java
+++ src/rest/AppConfig.java
@@ -1,8 +1,9 @@
 package rest;
 
+import org.glassfish.jersey.server.ResourceConfig;
 import javax.ws.rs.ApplicationPath;
-import javax.ws.rs.core.Application;
 
 @ApplicationPath("/rest")
-public class AppConfig extends Application {
+public class AppConfig extends ResourceConfig {
+
 }
