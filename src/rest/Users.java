3958a7b9602d5cdb48238ed5c758e8612b05b842
Anders Frandsen <anfran@anfran.dk>
2019-06-15 11:36:42 +0200

    some working rest

--- src/rest/Users.java
+++ src/rest/Users.java
@@ -10,6 +10,8 @@ import javax.ws.rs.core.Response;
 import java.util.List;
 
 @Path("users")
+@Consumes(MediaType.APPLICATION_JSON)
+@Produces(MediaType.APPLICATION_JSON)
 public class Users {
     BrugerLogic userLogic;
 
@@ -22,23 +24,58 @@ public class Users {
     }
 
     @GET
-    public List<BrugerDTO> getUserList() throws Exception {
+    public Response getUserList() {
         try {
             List<BrugerDTO> list = userLogic.getBrugerList();
-            return list;
+            return Response.status(200)
+                    .type(MediaType.APPLICATION_JSON_TYPE)
+                    .entity(list)
+                    .build();
         } catch (Exception ex) {
-            throw new Exception(ex);
+            return Response.status(400)
+                    .type(MediaType.TEXT_PLAIN)
+                    .build();
         }
     }
 
     @GET
     @Path("{id}")
-    public BrugerDTO getUser(@PathParam("id") int id) throws Exception {
+    public Response getUser(@PathParam("id") int id) {
         try {
             BrugerDTO user = userLogic.getBruger(id);
-            return user;
+            return Response.status(200)
+                    .type(MediaType.APPLICATION_JSON_TYPE)
+                    .entity(user)
+                    .build();
         } catch (Exception ex) {
-            throw new Exception(ex);
+            return Response.status(400)
+                    .type(MediaType.TEXT_PLAIN)
+                    .build();
+        }
+    }
+
+    @POST
+    public Response createUser(String body) {
+        JSONObject json = new JSONObject(body);
+
+        try {
+            BrugerDTO user = new BrugerDTO(
+                    json.getInt("userId"),
+                    json.getString("userName"),
+                    json.getString("ini"),
+                    json.getString("cprNumber"),
+                    json.getString("role")
+            );
+            userLogic.createBruger(user);
+            return Response.status(200)
+                    .type(MediaType.APPLICATION_JSON_TYPE)
+                    .entity(user)
+                    .build();
+        } catch (Exception ex) {
+            return Response.status(400)
+                    .type(MediaType.TEXT_PLAIN)
+                    .entity(ex.getMessage())
+                    .build();
         }
     }
 }
