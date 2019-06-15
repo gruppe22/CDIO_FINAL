ede3f018a435a6481308c2991991158828d222f3
Anders Frandsen <anfran@anfran.dk>
2019-06-15 11:36:04 +0200

    createBruger in BLL

--- src/logic/BrugerLogic.java
+++ src/logic/BrugerLogic.java
@@ -1,11 +1,12 @@
 package logic;
 
 import dao.BrugerDAO;
+import dao.IBrugerDAO;
 import dto.BrugerDTO;
 import java.util.List;
 
 public class BrugerLogic {
-    private BrugerDAO dao = new BrugerDAO();
+    private IBrugerDAO dao = new BrugerDAO();
 
     public BrugerDTO getBruger(int id) throws Exception {
         try {
@@ -19,6 +20,16 @@ public class BrugerLogic {
     public List<BrugerDTO> getBrugerList() throws Exception {
         try {
             return dao.getBrugerList();
+        }
+        catch (Exception ex) {
+            throw new Exception(ex);
+        }
+    }
+
+    public BrugerDTO createBruger(BrugerDTO user) throws Exception {
+        try {
+            dao.createBruger(user);
+            return dao.getBruger(user.getOprId());
         }
         catch (Exception ex) {
             throw new Exception(ex);
